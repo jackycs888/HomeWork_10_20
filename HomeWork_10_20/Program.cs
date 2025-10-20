ee65b2a698aea85105b10f15329e54aa1a3b3f83
Jason <jackycs888@gmail.com>
2025-10-20 10:15:05 +0800

    加入專案檔案。

---
+++ HomeWork_10_20/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWork_10_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
