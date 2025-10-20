6e2232b1eb8660fd336a46c3ca973c248b1b2de4
Jason <jackycs888@gmail.com>
2025-10-20 10:33:07 +0800

    First Commit

--- HomeWork_10_20/Program.cs
+++ HomeWork_10_20/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string number = Console.ReadLine();
+
+            int number2 = int.Parse(number);
+
+            for(int i = 1; i<number2; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Dann");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Build");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("School");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
